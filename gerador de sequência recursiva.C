33436e5681fa225f26b2da441fa4fd299771c559
leorezend <leonardorezende694@gmail.com>
2024-09-04 15:38:50 -0300

    primeiro commit

---
+++ gerador de sequência recursiva.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int f(int x);
+int main(int argc, char** argv){
+	int n,i;
+	printf("coloque um numero:");
+	scanf("%d",&n);
+	int vet[n];
+	for(i=0;i<n;i++){
+		vet[i]=f(i);
+	}
+	for(i=0;i<n;i++){
+		printf("%d,",vet[i]);
+	}
+	
+	return 0;
+}
+int f(int x){
+	if(x==0){
+		return 3;
+	}else{	
+		return 2*f(x-1);
+	}
+}
